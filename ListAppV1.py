a624f77eafff3befbb7505aff0b2524294be4111
ZP-DebugMaster <77686455+ZP-DebugMaster@users.noreply.github.com>
2021-03-12 12:57:56 -0600

    Add files via upload

--- ListAppV1.py
+++ ListAppV1.py
@@ -7,24 +7,30 @@ Program Goals:
 """
 
 #create functions that will preform those actions above
+import random
 myList = []
 
 def mainProgram():
     while True:
-        print("Hello, there! Let's work with lists!")
-        print("Choose one of the following options. Type a number ONLY")
-        choice = input("""  1. Add to list,
-2. Return the value at an index position
-3. End Program""")
-    if choice == "1":
-        addToList()
-    elif choice == "2":
-        indexValues()
-    elif choice == "3":
-        break
- 
-        
-    #we need ti add an exit program AAAAAAND error catching!
+        try:
+            print("Hello, there! Let's work with lists!")
+            print("Choose one of the following options. Type a number ONLY")
+            choice = input("""  1. Add to list,
+    2. Return the value at an index position
+    3. End Program""")
+        if choice == "1":
+            addToList()
+        elif choice == "2":
+            indexValues()
+        elif choice == "3":
+            randomSearch()
+        else:
+            break
+    except:
+        print("An error occurred")
+       
+       
+        #we need ti add an exit program AAAAAAND error catching!
 def addToList():
     newItem = input("Please type an integer!   ")
     myList.append(int(newItem))
@@ -36,7 +42,7 @@ def indexValues():
 
 def randomSearch():
     print("Here's a random value from your list!")
-    print(myList[random.randit(0, len(myList)-1])
+    print(myList[random.randit0, len(myList)-1)])
 
 if __name__== "__main__":
     mainProgram()
