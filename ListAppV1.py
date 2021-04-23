6c9cf77b6b062b2719212c597aa588186876bde6
ZP-DebugMaster <77686455+ZP-DebugMaster@users.noreply.github.com>
2021-04-23 11:35:23 -0600

    Add files via upload

--- ListAppV1.py
+++ ListAppV1.py
@@ -6,12 +6,23 @@ Program Goals:
 4. Pull values from specific index positions
 """
 
-#create functions that will preform those actions above
+"""
+Sometimes we want our computer to pick a random number in a given range, pick a random card from a deck, or flip a coin.
+The random module will provide access to functions that support these types of operations.
+"""
 import random
 myList = []
 unique_list = []
 
+
+"""
+def mainProgram is a function that is the starting point to any python program that you make. 
+"""
 def mainProgram():
+"""
+The while True loop means to run forever or runs ss long as an expression evaluates to true.
+Thats why you would need a beak statement to stop the loop.
+"""
     while True:
         print("Hello, there! Let's work with lists!")
         print("Choose one of the following options. Type a number ONLY")
@@ -25,7 +36,13 @@ def mainProgram():
 8. Iterative Binary Search
 9. Print List
 10. Quit   """)
-            if choice == "!":
+
+            """
+            The elif statement is python's way of saying, "if the previous conditions were not true, then try this condition."
+            An if statement is basically comparing two values that either have a true of false output. If its true it'll print yes and if its false it'll print no.
+            A break statement is used to end a loop, if its used without an argument it will end the function and returns to where the code was executing previously.
+            """
+            if choice == "1":
                 addToList()
             elif choice == "2":
                 addBunch()
@@ -52,13 +69,18 @@ def mainProgram():
             else:
                 break
     
-
+"""
+Lists are used to store multiple items in a single variable. In this list we will be adding numbers because we have
+input("Please type and integer! "). 
+"""
 def addToList():
     print("Adding to a list! Great choice!")
     newItem = input("Please type an integer!   ")
     myList.append(int(newItem))
     #Think about errors!
-
+"""
+This command just adds a lot of numbers and I mean a lot of numbers. 
+"""
 def addABunch():
     print("We're gonna add a bunch of numbersto your list!")
     numToAdd = input("How many intergers would you like to add?   ")
@@ -67,7 +89,9 @@ def addABunch():
         myList.append(random.randit(0, int(numRange)))
     print("Your list is now complete!")
 
-
+"""
+The sort method sorts the list ascending by default. 
+"""
 def sortList(myList):
     for x in myList:
         if x not in unique_list:
@@ -78,25 +102,35 @@ def sortList(myList):
         print(unique_list)
        
     
-
+"""
+An index value searches for a given element from the start of the list and returns the lowest index where the element appears.
+"""
 def indexValues():
     print("At what index position do you want to search?")
     indexPos = input("At what index position would you like to look?  ")
     print(myList[int(indexPos)])
-
+"""
+Random serach keeps generating a value until the best possible value is found, it will keep generating points until it finds the best one. 
+"""
 def randomSearch():
     print("Oh I'm so random lul!")
     print(myList[random.randit(0, len(myList)-1)])
     if len(unique_list) > 0:
         print(unique_list[random.randit(0, len(unique_list)-1)])
-
+"""
+The linear search function is an algorithm that will find a target value within a list,
+it sequentially checks each element of the list for the target value until a match is found or until all the elements have been searched.
+"""
 def linearSearch():
     print("We're going to go through this list one item at a time!")
     searchValue = input("What are you looking for?   ")
     for x in range(len(myList)):
         if myList[x] == int(searchValue):
             print("Your item is at index position {}".format(x))
-
+"""
+Recursive binary search searches for a item in a list just like iterative but instead of following instructions step by step in a loop,
+it just continues to call itself.
+"""
 def recursiveBinarySearch(unique_list, low, high, x):
     if high >= low:
         mid = (high + low) // 2
@@ -114,7 +148,9 @@ def recursiveBinarySearch(unique_list, low, high, x):
     else:
         print("Your number isn't here!")
 
-
+"""
+Iterative binary search uses loops to repeat a whole set of instructions instead of just calling itself over and over like recursive binary search. 
+"""
 def iterativeBinarySearch(unique_list, x):
     low = 0
     high = len(unique_list)-1
@@ -132,6 +168,9 @@ def iterativeBinarySearch(unique_list, x):
             return mid
     return -1
 
+"""
+
+"""
 def printLists():
     if len(unique_list) == 0:
         print(myList)
@@ -140,5 +179,9 @@ def printLists():
         if whichOne.lower() == "sorted":
             print(unique_list)
 
+
+"""
+
+"""
 if __name__== "__main__":
     mainProgram()
